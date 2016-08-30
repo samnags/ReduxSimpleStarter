f00d5e1fc88b5ea4ac3135e2dd380cecfee7dfff
samnags <sam.nagourney@gmail.com>
2016-08-30 15:26:56 -0400

    book-list

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export function selectBook(book) {
+  console.log('A book has been selected:', book.title);
+}
+
+// Need to get action to run through all of the reducers
+// Need to wrire action up to Redux
